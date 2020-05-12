998919836a5183919455bd78daf938b691009a64
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-05-12 18:52:27 +0200

    Save to csv file

--- Test.R
+++ Test.R
@@ -1,3 +1,6 @@
 dxffiles <- dir(pattern = "dxf")
-for(ttfile in dxffiles) 
-    dxfin(file = ttfile)
+for(ttfile in dxffiles) {
+    ttdxf <- dxfin(file = ttfile)
+    ttnewfilen <- unlist(strsplit(ttfile, "[.]"))
+    write.table(ttdxf, paste0(ttnewfilen[1], ".csv"), sep=",", col.names = FALSE, row.names = F, quot = F)
+}
