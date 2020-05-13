8632926414a7db41fdcfe01e05b9fb94e8621019
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-05-13 17:00:30 +0200

    File suffix changed

--- Test.R
+++ Test.R
@@ -2,5 +2,5 @@ dxffiles <- dir(pattern = "dxf")
 for(ttfile in dxffiles) {
     ttdxf <- dxfin(file = ttfile)
     ttnewfilen <- unlist(strsplit(ttfile, "[.]"))
-    write.table(ttdxf, paste0(ttnewfilen[1], ".csv"), sep=",", col.names = FALSE, row.names = F, quot = F)
+    write.table(ttdxf, paste0(ttnewfilen[1], ".EXP"), sep=",", col.names = FALSE, row.names = F, quot = F)
 }
