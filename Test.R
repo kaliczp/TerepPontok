26c05fb51c480df0e4f0fdbcec3dcbe881fc80e4
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-06-12 21:25:39 +0200

    Save result selected by attribute
    Different attribute index ared saved and used to select in the case
    of appropriate files.

--- Test.R
+++ Test.R
@@ -1,21 +1,21 @@
 dxffiles <- dir(pattern = "dxf")
 dxffiles <- dxffiles[c(1,3:10,2)]
-numcorr <- c(2:(-6),3)
+numcorr <- c(2:(-6),2)
 for(ttfile in 1:length(dxffiles)) {
     ttdxf <- dxfin(file = dxffiles[ttfile])
     ## Sorsz
-    Line.ssz <- ttdxf[,"K"] == "AP"
-    ttdxf[Line.ssz,1] <- sort(sample(1:9,2))*10
+    Line.ap <- ttdxf[,"K"] == "AP"
+    ttdxf[Line.ap,1] <- sort(sample(1:9,2))*10
     ttssznum <- numcorr[ttfile]
     Line.sp <- ttdxf[,"K"] == "SP"
     ttdxf[Line.sp, 1] <- ttdxf[Line.sp, 1] - ttssznum * 100
-    Line.sp <- ttdxf[,"K"] == "B" | ttdxf[,"K"] == "T"
-    ttdxf[Line.sp, 1] <- ttdxf[Line.sp,1] - ttssznum * 1000
+    Line.t <- ttdxf[,"K"] == "B" | ttdxf[,"K"] == "T"
+    ttdxf[Line.t, 1] <- ttdxf[Line.t,1] - (ttssznum - 2) * 1000 + 1
     ## Magasság
     ttdxf[,4] <- round(ttdxf[,4] +
                        runif(nrow(ttdxf), -0.32, 0.32) +
                        (ttfile - 1) * 15, 2)
     ttnewfilen <- unlist(strsplit(dxffiles[ttfile], "[.]"))
-    write.table(ttdxf[1:9,], paste0(ttnewfilen[1], "AP.EXP"), sep=",", col.names = FALSE, row.names = F, quot = F, eol = "\r\n")
-    write.table(ttdxf[10:91,], paste0(ttnewfilen[1], "RP.EXP"), sep=",", col.names = FALSE, row.names = F, quot = F, eol = "\r\n")
+    write.table(ttdxf[Line.ap | Line.sp,], paste0(ttnewfilen[1], "AP.EXP"), sep=",", col.names = FALSE, row.names = F, quot = F, eol = "\r\n")
+    write.table(ttdxf[Line.t,], paste0(ttnewfilen[1], "RP.EXP"), sep=",", col.names = FALSE, row.names = F, quot = F, eol = "\r\n")
 }
